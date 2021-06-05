85ca4a69a9ad143e10831caa35419316348c6c94
Nithin Saji <nithin111@gmail.com>
2021-06-05 15:03:17 +0700

    add untracked files

---
+++ trader.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class TradeEnv:
+    def __init__(self):
+        self.action_space = np.array([0, 1, 2])
+        pass
+
+    def step():
+        pass
+
+    def reset():
+        pass
+
+    def render():
+        pass
